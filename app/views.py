c58ac0d7a9ec75e652eb7b506cf754acc7a264ad
Devansh Shah <43884681+dev1911@users.noreply.github.com>
2020-07-25 19:08:28 +0530

    Fixed bug

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-
+from django.http import HttpResponse
 
 def trial(request):
-    if request.method == 'GET':
-        return "success"
+	if request.method == 'GET':
+		print("GETT")
+		return HttpResponse("<p> success </p>")
