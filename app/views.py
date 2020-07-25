dd0ae2bae4182a30c485ebd3c1622863098a74b9
Nehal-Surti <nehal.surti@spit.ac.in>
2020-07-25 22:14:30 +0530

    templates folder

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from app.templates import extract_url
+from .templates import extract_url
 
 def trial(request):
-	extract_url.extract()
-	return render(request, 'app/form.html')
+	url = extract_url.extract("the link to the location is https://www.google.com/maps/place/Charai+Hindu+Samshaan+Bhoomi/@19.0492292,72.8922557,18z/data=!4m5!3m4!1s0x3be7c8a8d4029427:0x991bade397542b3a!8m2!3d19.049521!4d72.8932652")
+	context = {'lati':url[0],'long':url[1]}
+	return render(request, 'app/form.html',context)
